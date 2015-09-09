9d028e0c8d642d6abd2f1f93a92145e421a4922f
Kevin Osborn <kevin.osborn@gmail.com>
2015-09-08 21:43:57 -0400

    initial commit of demo code for infinity mirror

---
+++ arduino/infinity-button/infinity-button.ino
@@ -0,0 +1,168 @@
+#include <Adafruit_NeoPixel.h>
+/* Demo code for infinity mirror. Starts with LED's off
+   but switches between modes on button press
+*/
+#define PIN A2 
+#define BUTTONPIN 3
+
+// Parameter 1 = number of pixels in strip
+// Parameter 2 = pin number (most are valid)
+// Parameter 3 = pixel type flags, add together as needed:
+//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
+//   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
+//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
+//   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(15, PIN, NEO_GRB + NEO_KHZ800);
+#define NUMMODES 8
+int mode;
+void setup() {
+  pinMode(BUTTONPIN,INPUT);
+  digitalWrite(BUTTONPIN,HIGH); //pullup
+  strip.begin();
+  strip.show(); // Initialize all pixels to 'off'
+}
+
+void loop() {
+  /*
+  // Send a theater pixel chase in...
+  theaterChase(strip.Color(127, 127, 127), 50); // White
+  theaterChase(strip.Color(127,   0,   0), 50); // Red
+  theaterChase(strip.Color(  0,   0, 127), 50); // Blue
+  */
+  checkButton();
+  if(!(mode%2)) //even numbers blank
+    blank();
+  else if (mode ==1)
+    rainbow(20);
+  else if (mode ==3)
+    rainbowCycle(20);
+  else if (mode == 5)
+    theaterChaseRainbow(50);
+  else if (mode == 7){
+    theaterChase(strip.Color(127, 127, 127), 50); // White
+    theaterChase(strip.Color(127,   0,   0), 50); // Red
+    theaterChase(strip.Color(  0,   0, 127), 50); // Blue
+  }
+  else
+  blank();
+}
+  
+#define BOUNCEDELAY 150
+long lastDebounceTime = 0L;
+int buttonState = HIGH;
+int lastButtonState = HIGH;
+boolean checkButton(){
+  boolean retval = false;
+  if ((millis() -lastDebounceTime) > BOUNCEDELAY){
+    int reading = digitalRead(BUTTONPIN);
+    if (reading != lastButtonState){
+      //reset the debounce timer
+      lastDebounceTime = millis();
+      if (reading == LOW){
+        // advance mode
+        mode = mode >=NUMMODES-1?0:mode+1;
+        retval = true; 
+      }
+    }
+    lastButtonState = reading;
+  }
+  return retval;
+}
+// Fill with black
+void blank(){
+  for (uint16_t i =0; i < strip.numPixels(); i++)
+    strip.setPixelColor(i,0);
+  strip.show();
+}
+// Fill the dots one after the other with a color
+void colorWipe(uint32_t c, uint8_t wait) {
+  for(uint16_t i=0; i<strip.numPixels(); i++) {
+      strip.setPixelColor(i, c);
+      strip.show();
+      delay(wait);
+  }
+}
+
+void rainbow(uint8_t wait) {
+  uint16_t i, j;
+
+  for(j=0; j<256; j++) {
+    for(i=0; i<strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel((i+j) & 255));
+      if (checkButton())
+        return;
+    }
+    strip.show();
+    delay(wait);
+  }
+}
+
+// Slightly different, this makes the rainbow equally distributed throughout
+void rainbowCycle(uint8_t wait) {
+  uint16_t i, j;
+
+  for(j=0; j<256*5; j++) { // 5 cycles of all colors on wheel
+    for(i=0; i< strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
+      if (checkButton())
+        return;
+    }
+    strip.show();
+    delay(wait);
+  }
+}
+
+//Theatre-style crawling lights.
+void theaterChase(uint32_t c, uint8_t wait) {
+  for (int j=0; j<10; j++) {  //do 10 cycles of chasing
+    for (int q=0; q < 3; q++) {
+      for (int i=0; i < strip.numPixels(); i=i+3) {
+        strip.setPixelColor(i+q, c);    //turn every third pixel on
+        if (checkButton())
+          return;
+      }
+      strip.show();
+     
+      delay(wait);
+     
+      for (int i=0; i < strip.numPixels(); i=i+3) {
+        strip.setPixelColor(i+q, 0);        //turn every third pixel off
+      }
+    }
+  }
+}
+
+//Theatre-style crawling lights with rainbow effect
+void theaterChaseRainbow(uint8_t wait) {
+  for (int j=0; j < 256; j++) {     // cycle all 256 colors in the wheel
+    for (int q=0; q < 3; q++) {
+        for (int i=0; i < strip.numPixels(); i=i+3) {
+          strip.setPixelColor(i+q, Wheel( (i+j) % 255));    //turn every third pixel on
+          if (checkButton())
+            return;
+        }
+        strip.show();
+       
+        delay(wait);
+       
+        for (int i=0; i < strip.numPixels(); i=i+3) {
+          strip.setPixelColor(i+q, 0);        //turn every third pixel off
+        }
+    }
+  }
+}
+
+// Input a value 0 to 255 to get a color value.
+// The colours are a transition r - g - b - back to r.
+uint32_t Wheel(byte WheelPos) {
+  if(WheelPos < 85) {
+   return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+  } else if(WheelPos < 170) {
+   WheelPos -= 85;
+   return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  } else {
+   WheelPos -= 170;
+   return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  }
+}
+
